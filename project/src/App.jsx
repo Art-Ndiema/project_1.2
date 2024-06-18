c205f19f717b8b4a520e80dbd86db5f64895317e
Art-Ndiema <art.ndiema@gmail.com>
2024-06-18 17:20:24 +0300

    projects.jsx has an error still looking at it

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -4,13 +4,15 @@ import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/skills';
 import { Projects } from './components/projects';
+//import { Projects } from './components/projects';
 function App() {
   return (
     <div className='App'>
      <NavBar/>
      <Banner/>
      <Skills/>
-    
+     
+  
     </div>
   );
 }
--- project/src/components/ProjectCard.jsx
+++ project/src/components/ProjectCard.jsx
@@ -1,16 +1,15 @@
-import { Col } from "react-bootstrap"
+import { Col } from "react-bootstrap";
 
-export const ProjectCard =({title, description, imgUrl}) => {
-    return(
-        <Col sm={5} md={3}>
-            <div className="proj-imgbx">
-                <img src={ imgUrl} alt=" project image" />
-            <div  className=" proj-txtx">
-                <h3> {title}</h3>
-                <span> {description}</span>
-
-            </div>
-            </div>
-        </Col>
-    )
+export const ProjectCard = ({ title, description, imgUrl }) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+      <div className="proj-imgbx">
+        <img src={imgUrl} />
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+      </div>
+    </Col>
+  )
 }
